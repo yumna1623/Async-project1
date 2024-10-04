38fd2543b0540d2991a22444a675d951122ff981
yumna1623 <yumnanasir1623@gmail.com>
2024-10-04 14:45:06 +0500

    setInterval

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 
 
 //-------------------------------------------setTimeout-------------------------------------------------
+
+{/* <h1>CHAI OR CODE</h1>
+    <button id="start">start</button>
+    <button id="stop">stop</button> */}
+
+
 // const sayHitesh= function () {
 //     console.log("hitesh");    
 // }
@@ -8,7 +14,7 @@
 //     document.querySelector('h1').innerHTML="best js playlist"
 // }
 // const changeMe=setTimeout(changeText,4000)
-// // ============================
+// ============================
 // document.querySelector('#stop')
 // .addEventListener('click',function () {
 //     clearTimeout(changeMe)
@@ -19,9 +25,17 @@
 
 //-------------------------------------------setInterval-------------------------------------------------
 
-const sayDate= function (str) {
-    console.log(str,Date.now());
+// const sayDate= function (str) {
+//     console.log(str,Date.now());
     
-}
-const intervalId=setInterval(sayDate,1000,"hi")
-clearInterval(intervalId)
+// }
+// const intervalId=setInterval(sayDate,1000,"hi")
+// clearInterval(intervalId)
+
+// console.log(intervalId);
+// document.querySelector('#stop')
+// .addEventListener('click',function () {
+//     clearTimeout(intervalId)
+//     console.log("STOPPED");
+
+// })
